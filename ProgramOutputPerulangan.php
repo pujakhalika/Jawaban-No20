a768d9cac02339acd3515ceb785ba65733e4f712
Puja Khalika <73125355+pujakhalika@users.noreply.github.com>
2021-10-10 05:22:53 +0700

    Add files via upload

---
+++ ProgramOutputPerulangan.php
@@ -0,0 +1,33 @@
+
+
+/* F5 */
+
+<?php 
+
+$ulangi = 5;
+
+do {
+    echo "<p>ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+} while ($ulangi > 1);
+
+for($i = 0; $i < 5; $i++){
+    echo "<h2>Ini perulangan ke-$i</h2>";
+}
+
+?>
+
+/* f3 */
+
+<?php 
+
+$ulangi = 3;
+
+do {
+    echo "<p>ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+} while ($ulangi > 1);
+
+for($i = 1; $i < 3; $i++){
+    echo "<h2>Ini perulangan ke-$i</h2>";
+}
